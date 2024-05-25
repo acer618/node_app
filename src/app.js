312bdb30d82d9743d37873896e4cb0d2dbea5f2a
Amit Mokal <amitmokal@gmail.com>
2024-05-25 16:02:08 -0400

    routes hard coded

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+
+app.get('/users', function (req, res) {
+  res.send([
+	{ id:1, name:'John' }, 
+	{ id:2, name:'Jane' },
+  ]);
+});
+
+app.get('/users/:id', function (req, res) {
+  const id = req.params.id; 
+  res.send({ id, name:'John'});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something went wrong' });
 });
