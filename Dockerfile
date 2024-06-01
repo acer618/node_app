45a52486d6e3504db008cda8fb0fa7c9db494f08
Amit Mokal <amitmokal@gmail.com>
2024-06-01 08:50:26 -0400

    docker-compose mysql, dotenv

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 WORKDIR /home/node/app 
 COPY package*.json ./
 USER node
-#RUN npm install
-#COPY --chown=node:node . .
-COPY . .
+RUN npm install
+COPY --chown=node:node . .
+#COPY . .
 EXPOSE 3000
-CMD ["node", "src/app.js"]
+CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var createPool = require('mysql2/promise');
+var config = require('dotenv');
+
 var app = express();
 
+const pool = createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_DOCKER_PORT,
+});
+
+pool.on("connection, () => console.log("DB Connected!"));
+
+app.get('/ping', function (req, res) {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result[0]);
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
 
 app.get('/users', function (req, res) {
   res.send([
@@ -19,9 +34,8 @@ app.get('/users/:id', function (req, res) {
   res.send({ id, name:'John'});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.NODE_DOCKER_PORT || 3000);
+console.log("Server on port", process.env.NODE_DOCKER_PORT || 3000);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
