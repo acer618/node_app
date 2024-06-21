8556db74c5998f3aaf422aad6f6bb512d9fc28c2
Amit Mokal <amitmokal@gmail.com>
2024-06-21 18:53:27 -0400

    sdk auto inst

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,7 +1,6 @@
 /*instrumentation.js*/
-// Require dependencies
+
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-//const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
@@ -10,16 +9,18 @@ const {
   OTLPTraceExporter,
 } = require('@opentelemetry/exporter-trace-otlp-proto');
 
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
 const sdk = new NodeSDK({
-  //traceExporter: new ConsoleSpanExporter(),
+  resource: new Resource({
+  	[SemanticResourceAttributes.SERVICE_NAME]: 'node_app',
+  }),
   traceExporter: new OTLPTraceExporter({
-    // optional - default url is http://localhost:4318/v1/traces
-    url: 'http://localhost:9411/v1/traces',
-    // optional - collection of custom headers to be sent with each request, empty by default
+    url: 'http://localhost:4318/v1/traces',
     headers: {},
   }), 
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start();
-
